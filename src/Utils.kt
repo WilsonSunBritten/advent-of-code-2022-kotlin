1f5060009aa2f0f29f53f35dcc78fa9c07e5a98f
wilson britten <wilsonsunbritten@gmail.com>
2022-12-01 22:17:49 -0700

    Day 2 solution

--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,40 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+fun <T> Collection<T>.splitBy(inclusionType: InclusionType = InclusionType.NONE, splitCondition: (T) -> Boolean): Sequence<List<T>> {
+    return sequence {
+        var currentList = mutableListOf<T>()
+        this@splitBy.forEach { entry ->
+            if (splitCondition(entry)) {
+                when (inclusionType) {
+                    InclusionType.NONE -> {
+                        // ignore this entry
+                        yield(currentList)
+                        currentList = mutableListOf()
+                    }
+                    InclusionType.LEFT -> {
+                        currentList.add(entry)
+                        yield(currentList)
+                        currentList = mutableListOf()
+                    }
+                    InclusionType.RIGHT -> {
+                        yield(currentList)
+                        currentList = mutableListOf(entry)
+                    }
+                }
+            } else {
+                currentList.add(entry)
+            }
+        }
+        if (currentList.isNotEmpty()) {
+            yield(currentList)
+        }
+    }
+}
+
+enum class InclusionType {
+    NONE,
+    LEFT,
+    RIGHT;
+}
